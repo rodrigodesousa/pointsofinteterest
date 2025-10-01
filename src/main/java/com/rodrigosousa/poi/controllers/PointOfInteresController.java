59f60c0ae60ca5937812a8b71e8b1c4c3186e529
Rodrigo <rodrigudesousa@gmail.com>
2025-10-01 16:26:17 -0400

    feat: endpoint list nearby points

--- src/main/java/com/rodrigosousa/poi/controllers/PointOfInteresController.java
+++ src/main/java/com/rodrigosousa/poi/controllers/PointOfInteresController.java
@@ -3,10 +3,12 @@ package com.rodrigosousa.poi.controllers;
 import com.rodrigosousa.poi.controllers.dto.CreatePointOfInterest;
 import com.rodrigosousa.poi.entities.PointOfInterest;
 import com.rodrigosousa.poi.repositories.PointOfInterestRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class PointOfInteresController {
@@ -18,10 +20,34 @@ public class PointOfInteresController {
     }
 
     @PostMapping("/points-of-interest")
-    public ResponseEntity<?> createPointOfInterest(@RequestBody CreatePointOfInterest body) {
+    public ResponseEntity<Void> createPointOfInterest(@RequestBody CreatePointOfInterest body) {
 
         repository.save(new PointOfInterest(body.name(), body.x(), body.y()));
 
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/points-of-interest")
+    public ResponseEntity<Page<PointOfInterest>> listPointOfInterest(@RequestParam(name="page", defaultValue = "0") Integer page,
+                                                                     @RequestParam(name="pageSize", defaultValue = "10") Integer pageSize) {
+
+        var body = repository.findAll(PageRequest.of(page, pageSize));
+
+        return ResponseEntity.ok(body);
+    }
+
+    @GetMapping("/nearby-points-of-interest")
+    public ResponseEntity<List<PointOfInterest>> nearbyPointOfInterest(@RequestParam(name="x") Long x,
+                                                                       @RequestParam(name="y") Long y,
+                                                                       @RequestParam(name="dmax") Long dmax) {
+
+        var xMin = x - dmax;
+        var xMax = x + dmax;
+        var yMin = y - dmax;
+        var yMax = y + dmax;
+
+        var body = repository.findNearMe(xMin, xMax, yMin, yMax);
+
+        return ResponseEntity.ok(body);
     }
 }
--- src/main/java/com/rodrigosousa/poi/repositories/PointOfInterestRepository.java
+++ src/main/java/com/rodrigosousa/poi/repositories/PointOfInterestRepository.java
@@ -2,8 +2,22 @@ package com.rodrigosousa.poi.repositories;
 
 import com.rodrigosousa.poi.entities.PointOfInterest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PointOfInterestRepository extends JpaRepository<PointOfInterest, Long> {
+
+    @Query("""
+            SELECT p FROM PointOfInterest p
+            WHERE (p.x >= :xMin AND p.x <= :xMax)
+            AND (p.y >= :yMin AND p.y <= :yMax)
+            """)
+    List<PointOfInterest> findNearMe(@Param("xMin") long xMin,
+                                     @Param("xMax") long xMax,
+                                     @Param("yMin") long yMin,
+                                     @Param("yMax") long yMax);
 }
